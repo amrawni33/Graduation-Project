379964e9b27b6f2de16249a5c67f60c2fb381f3c
Soad-Eslam <114537882+Soad-Eslam@users.noreply.github.com>
2024-04-21 03:25:22 +0200

    Add files via upload

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function favourites()
+    {
+        return $this->hasMany(Favourites::class);
+    }
+
+    public function lastViewedProducts()
+    {
+        return $this->hasMany(Last_Viewed_Products::class);
+    }
 }
